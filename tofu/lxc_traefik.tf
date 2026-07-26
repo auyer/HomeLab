1ddc2591733c5b7801d27eea556e1be149b6b546
Rafael Passos <rafael@rcpassos.me>
2026-07-26 11:14:23 -0300

    traefik: replace nginx-proxy-manager with traefik with central configuration
    Signed-off-by: Rafael Passos <rafael@rcpassos.me>

--- tofu/lxc_traefik.tf
+++ tofu/lxc_traefik.tf
@@ -1,7 +1,7 @@
-resource "proxmox_lxc" "proxy" {
+resource "proxmox_lxc" "traefik" {
   target_node  = var.proxmox_node
-  vmid         = 102
-  hostname     = "proxy"
+  vmid         = 202
+  hostname     = "traefik"
   ostemplate   = data.external.debian_template.result.path
   unprivileged = true
   onboot       = true
@@ -15,13 +15,13 @@ resource "proxmox_lxc" "proxy" {
 
   rootfs {
     storage = "nvme512"
-    size    = "8G"
+    size    = "6G"
   }
 
   network {
     name     = "eth0"
     bridge   = var.nic_name
-    ip       = "10.255.0.102/22"
+    ip       = "10.255.2.2/22"
     gw       = "10.255.0.1"
     firewall = true
   }
